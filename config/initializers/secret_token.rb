fe397e0d358f3622454073f627a4e1d7ed2d03a1
ginok <miragino@gmail.com>
2012-03-05 22:41:07 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MjTsumotterServer::Application.config.secret_token = 'a5ef4d0567c1f06a51e94ec81e244b0a5ac84a8f3b4886cabb21f9ab1c825ebeaaae15b57b2c667f7e13a34a22dfb552b06aab7f5e3519679da8dc47721dd18c'
